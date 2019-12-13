d39ba18f5f829ff21780817c5047a3db290db99e
Dan Foley <admin@cantremember.com>
2019-12-13 14:36:04 -0800

    reasonable

--- src/callback-queue.js
+++ src/callback-queue.js
@@ -1,50 +1,37 @@
-"use strict";
-
 const Promise = require('bluebird');
-
 const callbackQueue = require('callback-queue');
+const { serializeError, deserializeError } = require('serialize-error');
 
-const {
-  serializeError,
-  deserializeError
-} = require('serialize-error'); // callback buckets
-
-
+// callback buckets
 const queue = new Map();
-const {
-  isArray
-} = Array;
+const { isArray } = Array;
+
 /**
  * Call functions stored in local queues
  * @param  {String} queueName
  * @param  {Array} args
  */
-
 async function call(queueName, args, logger) {
   const callback = queue.get(queueName);
-
   if (!callback) {
     throw new Error('callback called multiple times');
-  } // these are async anyways - gonna schedule them
+  }
 
-
+  // these are async anyways - gonna schedule them
   logger.debug('Calling %s callback with args', queueName, args);
-const arg = args[0];
-// console.log('--- queue', arg && arg.name)
-  callback(...args); // clean local queue
+  callback(...args);
 
+  // clean local queue
   queue.delete(queueName);
 }
+
 /**
  * Add callback into local queue
  * @param {String} key - queue key
  * @param {Function} callback - function to add
  */
-
-
 exports.add = function add(key, callback) {
   const aggregator = callbackQueue.add(key, callback);
-
   if (!aggregator) {
     return false;
   }
@@ -52,95 +39,91 @@ exports.add = function add(key, callback) {
   queue.set(key, aggregator);
   return true;
 };
+
 /**
  * Creates publish function that is used later on to process callbacks
  * @param {Object} redis
  */
-
-
 exports.createPublisher = function createPublisher(redis, pubsubChannel, logger) {
   return async function publishResult(lockRedisKey, err, ...args) {
     const broadcastArgs = [err ? serializeError(err) : null, ...args];
-    const localArgs = [err, ...args]; // post result to other processes
+    const localArgs = [err, ...args];
 
-    redis.publish(pubsubChannel, JSON.stringify([lockRedisKey, broadcastArgs])).catch(e => {
-      logger.warn({
-        err: e
-      }, 'failed to publish results');
-    }); // call local queue for faster processing
+    // post result to other processes
+    redis
+      .publish(pubsubChannel, JSON.stringify([lockRedisKey, broadcastArgs]))
+      .catch((e) => {
+        logger.warn({ err: e }, 'failed to publish results');
+      });
+
+    // call local queue for faster processing
     // we don't care here if it fails, it could've been already processed
-
     try {
       await call(lockRedisKey, localArgs, logger);
     } catch (e) {
-      logger.warn({
-        err: e,
-        lockRedisKey
-      }, 'failed to perform call');
+      logger.warn({ err: e, lockRedisKey }, 'failed to perform call');
     }
   };
 };
+
 /**
  * Helper function to parse possible JSON from the Buffer
  */
-
-
 function tryParsing(message, logger) {
   try {
     return JSON.parse(message);
   } catch (e) {
-    logger.warn({
-      message
-    }, 'Cant parse message');
+    logger.warn({ message }, 'Cant parse message');
     return null;
   }
 }
+
 /**
  * Creates publish function that is used later on to process callbacks
  * @param {Object} redis
  */
-
-
 exports.createConsumer = function createConsumer(redis, pubsubChannel, logger) {
-  const connect = () => redis.subscribe(pubsubChannel).tap(() => {
-    logger.info('Subscribed to channel %s', pubsubChannel);
-  }).catch(err => {
-    logger.fatal('Failed to subsctibe to pubsub channel:', err);
-    return Promise.delay(250).then(connect);
-  }); // init connection
+  const connect = () => redis
+    .subscribe(pubsubChannel)
+    .tap(() => {
+      logger.info('Subscribed to channel %s', pubsubChannel);
+    })
+    .catch((err) => {
+      logger.fatal('Failed to subsctibe to pubsub channel:', err);
+      return Promise.delay(250).then(connect);
+    });
 
-
+  // init connection
   connect();
+
   return async function redisEventListener(channel, _message) {
     if (channel.toString() !== pubsubChannel) {
       return;
     }
 
     const message = tryParsing(_message, logger);
-
     if (!isArray(message)) {
       return;
     }
 
     const [key, args] = message;
-
     if (!key || !isArray(args)) {
       logger.warn('Malformed message passed: no key or args.', message);
       return;
-    } // no listeners here
+    }
+
+    // no listeners here
     // eat the error
-
-
     try {
       if (args[0]) args[0] = deserializeError(args[0]);
       await call(key, args, logger);
     } catch (err) {
-      logger.warn({
-        err
-      }, 'call failed');
+      logger.warn({ err }, 'call failed');
     }
   };
-}; // //////////////////////////////////////////////////////////////////////////////
+};
+
+// //////////////////////////////////////////////////////////////////////////////
 // Private section
 // //////////////////////////////////////////////////////////////////////////////
 
@@ -149,6 +132,4 @@ exports.createConsumer = function createConsumer(redis, pubsubChannel, logger) {
  * used for testing
  * @type {Function}
  */
-
-
 exports._call = call;
--- src/distributed-callback-queue.js
+++ src/distributed-callback-queue.js
@@ -1,51 +1,33 @@
-"use strict";
-
 const Promise = require('bluebird');
-
 const redislock = require('ioredis-lock');
-
 const Redis = require('ioredis');
-
 const bunyan = require('bunyan');
+const assert = require('assert');
 
-const assert = require('assert'); // may only use redis with bluebird promise
+// may only use redis with bluebird promise
+Redis.Promise = Promise;
 
-
-Redis.Promise = Promise; // lodash helpers
-
+// lodash helpers
 const assign = require('lodash/assign');
-
 const defaults = require('lodash/defaults');
-
 const flatten = require('lodash/fp/flatten');
-
 const filter = require('lodash/fp/filter');
+const compose = require('lodash/fp/compose');
 
-const compose = require('lodash/fp/compose'); // internal deps
-
-
+// internal deps
 const callbackQueue = require('./callback-queue');
-
 const Semaphore = require('./semaphore');
-
-const {
-  MultiLock,
-  MultiLockError
-} = require('./multi-lock');
-
+const { MultiLock, MultiLockError } = require('./multi-lock');
 const pkg = require('../package.json');
 
-const {
-  LockAcquisitionError
-} = redislock;
+const { LockAcquisitionError } = redislock;
 const isBoolean = filter(Boolean);
 const toFlattenedTruthyArray = compose(isBoolean, flatten);
 const couldNotAcquireLockError = new LockAcquisitionError('job is already running');
 const TimeoutError = new Promise.TimeoutError('queue-no-response');
+const notLockAcquisitionError = (e) => e.name !== 'LockAcquisitionError';
+const isTimeoutError = (e) => e === TimeoutError;
 
-const notLockAcquisitionError = e => e.name !== 'LockAcquisitionError';
-
-const isTimeoutError = e => e === TimeoutError;
 /**
  * @class DistributedCallbackQueue
  *
@@ -63,52 +45,45 @@ const isTimeoutError = e => e === TimeoutError;
  *        @param {String|Object} preset - either name of preset or streams obj for bunyan
  *    @param {String} lockPrefix - used for creating locks in redis
  */
-
-
 class DistributedCallbackQueue {
   constructor(options = {}) {
-    const {
-      client
-    } = options;
+    const { client } = options;
     assert.ok(client, 'options.client must be defined');
-    const pubsub = options.pubsub || (typeof client.duplicate === 'function' ? client.duplicate({
-      lazyConnect: false
-    }) : client);
 
+    const pubsub = options.pubsub || (typeof client.duplicate === 'function' ? client.duplicate({ lazyConnect: false }) : client);
     if (!(pubsub instanceof Redis.Cluster)) {
       assert.notStrictEqual(client, pubsub, 'options.client and options.pubsub must have separate redis clients');
     }
 
-    const {
-      pubsubChannel
-    } = options;
+    const { pubsubChannel } = options;
     assert.ok(pubsubChannel, 'pubsubChannel must be specified');
+
     const lockOptions = defaults(options.lock || {}, {
       timeout: 10000,
       retries: 2,
-      delay: 100
+      delay: 100,
     });
-    const logger = this.logger = DistributedCallbackQueue.initLogger(options); // put on the instance
 
+    const logger = this.logger = DistributedCallbackQueue.initLogger(options);
+
+    // put on the instance
     assign(this, {
       client,
       pubsub,
       lockOptions,
       lockPrefix: options.lockPrefix || pkg.name,
       publish: callbackQueue.createPublisher(client, pubsubChannel, logger),
-      consume: callbackQueue.createConsumer(pubsub, pubsubChannel, logger)
+      consume: callbackQueue.createConsumer(pubsub, pubsubChannel, logger),
     });
-    pubsub.on('messageBuffer', this.consume); // ready
 
+    pubsub.on('messageBuffer', this.consume);
+
+    // ready
     this.logger.info('Initialized...');
   }
 
   static initLogger(options) {
-    const {
-      log: logger,
-      debug,
-      name
-    } = options;
+    const { log: logger, debug, name } = options;
     const loggerEnabled = typeof logger === 'undefined' ? !!debug : logger;
 
     if (loggerEnabled && logger instanceof bunyan) {
@@ -118,38 +93,34 @@ class DistributedCallbackQueue {
     const streams = [{
       level: 'trace',
       type: 'raw',
-      stream: new bunyan.RingBuffer({
-        limit: 100
-      })
+      stream: new bunyan.RingBuffer({ limit: 100 }),
     }];
 
     if (loggerEnabled) {
       streams.push({
         stream: process.stdout,
-        level: debug ? 'debug' : 'info'
+        level: debug ? 'debug' : 'info',
       });
     }
 
     return bunyan.createLogger({
       name: name || pkg.name,
-      streams
+      streams,
     });
   }
+
   /**
    * Combines task key
    * @param  {String} suffix
    */
-
-
   key(suffix) {
     return `${this.lockPrefix}${suffix}`;
   }
+
   /**
    * Creates lock instance
    * @return {Lock}
    */
-
-
   getLock() {
     return redislock.createLock(this.client, this.lockOptions);
   }
@@ -162,51 +133,51 @@ class DistributedCallbackQueue {
    * @returns {Promise} if promise is resolved then we must act, if it's rejected - then
    *                    somebody else is working on the same task right now
    */
-
-
   async push(suffix, next, timeout = this.lockOptions.timeout * 2) {
-    assert(suffix, 'must be a truthy string'); // first queue locally to make use of pending requests
+    assert(suffix, 'must be a truthy string');
 
+    // first queue locally to make use of pending requests
     const lockRedisKey = this.key(suffix);
-    const queued = callbackQueue.add(lockRedisKey, next); // this means that we already have local callback queue with that
-    // identifier, don't try to lock it again and proceed further
+    const queued = callbackQueue.add(lockRedisKey, next);
 
+    // this means that we already have local callback queue with that
+    // identifier, don't try to lock it again and proceed further
     if (!queued) {
-// console.warn('--- push couldNotAcquireLockError');
       throw couldNotAcquireLockError;
     }
 
     if (timeout) {
       /* we are first in the local queue */
-      const onTimeout = setTimeout(callbackQueue._call, timeout, lockRedisKey, [TimeoutError], this.logger);
+      const onTimeout = setTimeout(
+        callbackQueue._call,
+        timeout,
+        lockRedisKey,
+        [TimeoutError],
+        this.logger
+      );
+
       /* if we have no response from dlock -> without timeout, clean local queue */
-
       callbackQueue.add(lockRedisKey, () => clearTimeout(onTimeout));
-    } // create lock
+    }
 
+    // create lock
+    const lock = this.getLock();
 
-    const lock = this.getLock(); // get the lock
-
+    // get the lock
     try {
-// console.warn('--- lock A')
       await lock.acquire(lockRedisKey);
-// console.warn('--- lock B')
       return await this.createWorker(lockRedisKey, lock);
     } catch (e) {
       if (notLockAcquisitionError(e)) {
         // this is an abnormal error, need to post it and cancel requests
         // so that they dont hang
-// console.warn('--- push publish', e.name);
         await this.publish(lockRedisKey, e);
       }
 
-// console.warn('--- push throw', e.name);
       throw e;
     }
-    finally {
-// console.warn('--- lock C')
-    }
   }
+
   /**
    * Provides a helper over push method to be able to perform the same
    * work using promises with async/await style
@@ -217,8 +188,6 @@ class DistributedCallbackQueue {
    *
    * @return {Function} job handler that must be invoked with a worker that returns a promise
    */
-
-
   async fanout(suffix, ...props) {
     // verify we've got correct args in
     assert(props.length >= 1 && props.length <= 2);
@@ -227,79 +196,71 @@ class DistributedCallbackQueue {
     assert(typeof worker === 'function', 'ensure that you pass a function as a worker');
 
     let jobAbortReject;
-    let jobAbortPromise = new Promise((_resolve, reject) => {
+    let jobAbortPromise = new Promise((resolve, reject) => {
       jobAbortReject = reject;
     });
 
     let onJobCompleted;
-    const promise = new Promise((resolve, reject) => {
+    const jobCompletedPromise = new Promise((resolve, reject) => {
       onJobCompleted = (err, ...args) => {
         if (err) {
-// console.warn('--- onJobCompleted err', err.name);
           if (jobAbortPromise) {
             jobAbortReject(err);
           }
           setImmediate(() => {
             reject(err);
-          })
+          });
           return;
         }
 
         resolve(...args);
       };
     });
-    let pushPromise;
-    let onCompleted;
 
-// console.warn('--- onJobCompleted ...');
+    let onCompleted;
     try {
-      // bluebird Promise for "cancelling"
-      pushPromise = this.push(suffix, onJobCompleted, timeout);
-      onCompleted = await Promise.race([ pushPromise, jobAbortPromise ]);
-// console.warn('--- onJobCompleted : pushed');
+      const pushPromise = this.push(suffix, onJobCompleted, timeout);
+      onCompleted = await Promise.race([pushPromise, jobAbortPromise]);
       jobAbortPromise = undefined;
     } catch (err) {
-// console.warn('--- onJobCompleted push#catch', err.name);
       if (notLockAcquisitionError(err)) {
-// console.warn('--- onJobCompleted => A');
         setImmediate(onJobCompleted, err);
-        return promise;
+        return jobCompletedPromise;
       }
 
-// console.warn('--- onJobCompleted => B');
-      return promise;
-    } // wrap so that we have concept of "cancelling" work
+      return jobCompletedPromise;
+    }
 
-
+    // wrap so that we have concept of "cancelling" work
     const performWork = worker();
 
     try {
-      const result = await Promise.race([performWork, promise]);
-// console.warn('--- onJobCompleted : race');
+      const result = await Promise.race([
+        performWork,
+        jobCompletedPromise,
+      ]);
+
       setImmediate(onCompleted, null, result);
     } catch (err) {
-// console.warn('--- onJobCompleted performWork#catch', err.name);
       // only local timeouts would trigger this as we do strict equality
       // if there is cancel method available on the job - we'll call it
       if (isTimeoutError(err) && typeof performWork.cancel === 'function') {
-// console.warn('--- onJobCompleted => C');
         performWork.cancel();
-      } // broadcast this everywhere so that others dont wait for longer than needed
+      }
+
+      // broadcast this everywhere so that others dont wait for longer than needed
       // as the result will fail either way
-
-
-// console.warn('--- onJobCompleted => D');
       setImmediate(onCompleted, err);
-    } // in some cases may already be resolved/rejected
+    }
 
-
-// console.warn('--- onJobCompleted : return');
-    return promise;
+    // in some cases may already be resolved/rejected
+    return jobCompletedPromise;
   }
 
   semaphore(bucket) {
     return new Semaphore(this, bucket);
   }
+
   /**
    * Performs task once and _does_ not notify others of it's completion
    * @param {String} suffix - queue identifier
@@ -309,72 +270,84 @@ class DistributedCallbackQueue {
    *                    Promise contains lock, which must be released after the job is completed
    *                    Call `lock.release()` or `lock.extend` based on what's needed
    */
-
-
   once(suffix) {
     assert(suffix, 'must be a truthy string');
+
     const lockRedisKey = this.key(suffix);
     const lock = this.getLock();
-    return lock.acquire(lockRedisKey).return(lock);
+    return lock
+      .acquire(lockRedisKey)
+      .return(lock);
   }
+
   /**
    * Acquires multi-lock. All or none strategy
    * @param  {String[]} args - array of locks to acquire
    * @return {MultiLock}
    */
-
-
   multi(...args) {
     const actions = toFlattenedTruthyArray(args);
     assert(actions.length, 'at least 1 action must be supplied');
-    return Promise.map(actions, action => this.once(action).reflect()).then(MultiLock.batchAction).catch(MultiLockError, MultiLock.cleanup).then(locks => new MultiLock(locks));
+
+    return Promise
+      .map(actions, (action) => this.once(action).reflect())
+      .then(MultiLock.batchAction)
+      .catch(MultiLockError, MultiLock.cleanup)
+      .then((locks) => new MultiLock(locks));
   }
+
   /**
    * Creates real queue, which performs operations in the order they were added to it
    * @param  {string} lockKey - Identifier, based on which we create the lock.
    * @param  {Function(completed)} jobFunction - Accepts one arg, which is a fn, which must be called when work is done.
    * @returns {Promise<*>}
    */
-
-
   serial(lockKey, jobFunction) {
-    const workUnit = next => {
+    const workUnit = (next) => {
       let called = 0;
       let rejected = false;
 
-      const done = err => {
+      const done = (err) => {
         // in case there are some remnants of this
-        if (called === true) return null; // increase counter for further queueing
+        if (called === true) return null;
 
-        called += 1; // error handling
+        // increase counter for further queueing
+        called += 1;
 
+        // error handling
         if (err) {
           // if we failed to acquire lock - do a noop
           // and record failure of lock acquisition
           if (err instanceof LockAcquisitionError) {
             rejected = true;
             return null;
-          } // if it's not an acquisition error - then it's operational
+          }
+
+          // if it's not an acquisition error - then it's operational
           // and we must end early with an error
-
-
           return next(err);
-        } // in-case that is not an error and call counter is 1 - simply return
+        }
+
+        // in-case that is not an error and call counter is 1 - simply return
         // we must wait for the second call
+        if (called === 1) return null;
 
+        // if we were not rejected - return control
+        if (rejected === false) return next();
 
-        if (called === 1) return null; // if we were not rejected - return control
-
-        if (rejected === false) return next(); // try requeueing and basically repeating operation until it succeeds
-
+        // try requeueing and basically repeating operation until it succeeds
         return Promise.fromCallback(workUnit).asCallback(next);
       };
 
-      return this.push(lockKey, done).then(jobFunction).asCallback(done);
+      return this
+        .push(lockKey, done)
+        .then(jobFunction)
+        .asCallback(done);
     };
 
     return Promise.fromCallback(workUnit);
   }
+
   /**
    * Returns function that should be called with the result of the work
    * @param {String} lockRedisKey - key used for locking
@@ -382,8 +355,6 @@ class DistributedCallbackQueue {
    * @returns {Function} worker - call with arguments that need to be passed to
    *                              all queued callbacks
    */
-
-
   createWorker(lockRedisKey, acquiredLock) {
     /**
      * This function must be called when job has been completed
@@ -392,23 +363,23 @@ class DistributedCallbackQueue {
      */
     const broadcastJobStatus = async (err, ...args) => {
       /* clen ref */
-      const {
-        lock
-      } = broadcastJobStatus; // because a job may take too much time, other listeners must implement timeout/retry strategy
+      const { lock } = broadcastJobStatus;
 
+      // because a job may take too much time, other listeners must implement timeout/retry strategy
       if (lock == null) {
         this.logger.error('lock was already released');
         return null;
-      } // clean ref
+      }
 
+      // clean ref
+      broadcastJobStatus.lock = null;
 
-      broadcastJobStatus.lock = null; // must release lock now. Technically there could be an event
+      // must release lock now. Technically there could be an event
       // where lock had not been released, notification already emitted
       // and callback is stuck in the queue, to avoid that we can add retry
       // to lock acquisition. Desicion and constraints are up to you. Ideally
       // you would want to cache result of the function for some time - and then
       // this race is completed. Multi() command is not possible to use here
-
       try {
         // ensure lock still belongs to us
         await lock.extend();
@@ -416,44 +387,42 @@ class DistributedCallbackQueue {
         // because a job may take too much time, other listeners must implement timeout/retry strategy
         this.logger.warn('failed to release lock and publish results', error);
         return null;
-      } // emit event
+      }
+
+      // emit event
       // at this point we are sure that this job still belongs to us,
       // if it doesn't - we can't publish response, because this task may be acquired
       // by someone else
-
-
       try {
         return await this.publish(lockRedisKey, err, ...args);
       } finally {
         /* ensure we release the lock once publish is completed */
-
         /* during race conditions we rely on _retry_ setting to re-acquire lock */
         lock.release().reflect();
       }
-    }; // set associated lock -> lengthy jobs must extend this
+    };
 
-
+    // set associated lock -> lengthy jobs must extend this
     broadcastJobStatus.lock = acquiredLock;
+
     return broadcastJobStatus;
   }
-
 }
+
 /**
  * Constructor for distributed callback queue
  * @type {DistributedCallbackQueue}
  */
-
-
 module.exports = exports = DistributedCallbackQueue;
+
 /**
  * Expose custom error type for MultiLock
  * @type {MultiLockError}
  */
-
 exports.MultiLockError = MultiLockError;
+
 /**
  * Exposes MultiLock class
  * @type {MultiLock}
  */
-
 exports.MultiLock = MultiLock;
